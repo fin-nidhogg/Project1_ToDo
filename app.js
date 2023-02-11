a2fafd2a636e2b87218247ace2a839a2a89afa9d
nidhogg <aripekka.kantola@gmail.com>
2023-02-12 01:18:59 +0200

    General comment cleanup, Edit function added

--- app.js
+++ app.js
@@ -2,8 +2,16 @@
 const taskForm = document.getElementById("task-form");
 const todolist = document.getElementsByClassName("tasks-list");
 const taskInput = document.getElementById("task-input");
+const errorMessage = "What's the point of empty task?";
 
+// Main Array witch contains all tasks
 let tasks = JSON.parse(localStorage.getItem("todos")) || [];
+
+/* 
+///////////////////////////////
+"MAIN PROGRAM CODE STARTS HERE"
+///////////////////////////////
+*/
 
 // Read and render all elements from local storage
 if (localStorage.getItem("todos")) {
@@ -12,7 +20,10 @@ if (localStorage.getItem("todos")) {
   });
 }
 
-// Listen if submit button is pressed
+/*
+Listen if submit button is pressed. Default reload for submit is prevented.
+Catch form data, create object from it and save object into localstorage.
+*/
 taskForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
@@ -27,14 +38,10 @@ taskForm.addEventListener("submit", (e) => {
   const todo = {
     id: new Date().getTime(),
     name: taskInput.value,
-    ifCompleted: false,
+    done: false,
   };
 
-  /* 
-Save todo object into local storage. 
-Render new li -item from object
-*/
-
+  // Save todo object into local storage and render new li -item from object
   tasks.push(todo);
   localStorage.setItem("todos", JSON.stringify(tasks));
   createTodo(todo);
@@ -46,25 +53,62 @@ Render new li -item from object
   taskForm.focus();
 });
 
+// Event listener for triggering delete function for specified todoID
 todolist[0].addEventListener("click", (event) => {
-  if (event.target.classList.contains('delete') || 
-  event.target.parentElement.classList.contains('delete')) {
+  if (
+    event.target.classList.contains("delete") ||
+    event.target.parentElement.classList.contains("delete")
+  ) {
     const todoId = event.target.closest("li").id;
 
     deleteTodo(todoId);
   }
 });
 
-// Function to create and render li element from object
+// Event listener for triggering edit function for specified todoID
+todolist[0].addEventListener("input", (event) => {
+  const todoId = event.target.closest("li").id;
+
+  updateTodo(todoId, event.target);
+});
+
+// Remove focus from any element if [Enter] is pressed
+todolist[0].addEventListener("keydown", (event) => {
+  if (event.keyCode == 13) {
+    event.preventDefault();
+    event.target.blur();
+  }
+});
+
+/* 
+///////////////////////////////
+"MAIN PROGRAM CODE ENDS HERE"
+///////////////////////////////
+*/
+
+//////////////////////////////////
+// Function to CREATE new Todo Element
+//////////////////////////////////
 
 function createTodo(todo) {
   const todoElement = document.createElement("li");
   todoElement.setAttribute("id", todo.id);
 
+  if (todo.done == true) {
+    todoElement.classList.add("done");
+  }
+
+  if (todo.name == "What's the point of empty task?") {
+    todoElement.closest("li").classList.add("error");
+    todoElement.textContent = "What's the point of empty task?";
+  }
+
   const taskHtml = `
     <div>
-        <input type="checkbox" name="task" id="${todo.id}">
-        <span contenteditable="true">${todo.name}</span>
+        <input type="checkbox" name="task" id="${todo.id}" ${
+    todo.done ? "checked" : ""
+  }>
+        <span ${!todo.done ? "contenteditable" : ""}>${todo.name}</span>
     </div>
     <button title="Delete the "${todo.name}" task" class="delete">
     <i class="fa-sharp fa-solid fa-trash"></i>  
@@ -74,6 +118,10 @@ function createTodo(todo) {
   todolist[0].appendChild(todoElement);
 }
 
+//////////////////////////////////
+// Fuction to DELETE selected todo
+//////////////////////////////////
+
 function deleteTodo(todoId) {
   tasks = tasks.filter((todo) => {
     tasks.id != parseInt(todoId);
@@ -82,3 +130,44 @@ function deleteTodo(todoId) {
   localStorage.setItem("todos", JSON.stringify(tasks));
   document.getElementById(todoId).remove();
 }
+
+//////////////////////////////////
+// Fuction to EDIT selected todo
+//////////////////////////////////
+
+function updateTodo(todoId, todoElement) {
+  const todo = tasks.find((todo) => todo.id == parseInt(todoId));
+  const span = todoElement.nextElementSibling;
+  const parent = todoElement.closest("li");
+
+  if (todoElement.hasAttribute("contenteditable")) {
+    if (todoElement.textContent !== "") {
+      todo.name = todoElement.textContent; // IF Element is editable, edit content
+      parent.classList.remove("error");
+    } else {
+      todo.name = errorMessage;
+      todoElement.textContent = errorMessage;
+      parent.classList.add("error");
+    }
+  } else {
+    todo.done = !todo.done; // LI Element wasn't editable so event was triggered by tickbox. Therefore let's switch the state of "done" -value.
+
+    if (todo.done == true) {
+      // IF task has value done = true, remove ability to edit and add done css class.
+      span.removeAttribute("contenteditable");
+      parent.classList.remove("error");
+      parent.classList.add("done");
+    } else {
+      // Task isn't done so we can edit it = make field editable and remove possible done css class.
+      span.setAttribute("contenteditable", "true");
+      parent.classList.remove("done");
+    }
+  }
+  localStorage.setItem("todos", JSON.stringify(tasks));
+}
+
+/* 
+//////////////////////////////////
+FUNCTIONS USED IN MAIN LOGIC ENDS
+//////////////////////////////////
+*/
